387b339939fbbd75fe9611f4c6a3cfc134bce5fa
Sjarj <sebastien.jarjanette@icloud.com>
2019-12-09 17:17:57 +0100

    Actions and reducer of buttons

--- src/action/quiz.ts
+++ src/action/quiz.ts
@@ -14,10 +14,31 @@ export const getQuizListItem = (
   questionAmount: number,
   difficulty: Difficulty
 ) => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
+  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
     const r = await axios.get<IQuizList>(
       `https://opentdb.com/api.php?amount=${questionAmount}&difficulty=${difficulty}&type=boolean`
     );
     dispatch({ type: TYPES.getQuizListItems, payload: r.data.results });
+  };
+};
+
+export const giveAnswer = (
+  isCorrectAnswer: boolean,
+  isLastQuestion: boolean
+) => {
+  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
+    if (isCorrectAnswer) {
+      dispatch({ type: TYPES.incrementScore });
+    }
+    if (!isLastQuestion) {
+      dispatch({ type: TYPES.setNextQuestion });
+    }
+  };
+};
+
+export const restart = () => {
+  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
+    dispatch({ type: TYPES.restart });
+    dispatch(getQuizListItem(10, Difficulty.easy));
   };
 };
--- src/reducers/quiz.ts
+++ src/reducers/quiz.ts
@@ -3,10 +3,14 @@ import { TYPES } from './../action/action-types';
 
 export interface IQuizIOnitialState {
   quizListItem: IQuizListItem[];
+  currentQuizItemIndex: number;
+  score: number;
 }
 
 const quizInitalState: IQuizIOnitialState = {
-  quizListItem: []
+  quizListItem: [],
+  currentQuizItemIndex: 0,
+  score: 0
 };
 export const QuizReducer = (
   state = quizInitalState,
@@ -17,6 +21,22 @@ export const QuizReducer = (
       return {
         ...state,
         quizListItem: (action as Action<IQuizListItem[]>).payload
+      };
+    case TYPES.incrementScore:
+      return {
+        ...state,
+        score: state.score + 1
+      };
+    case TYPES.setNextQuestion:
+      return {
+        ...state,
+        currentQuizItemIndex: state.currentQuizItemIndex + 1
+      };
+    case TYPES.restart:
+      return {
+        ...state,
+        currentQuizItemIndex: 0,
+        score: 0
       };
 
     default:
